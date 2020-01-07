cb571d92f35836e3d097cccb587321b61b77107e
Somnath Paul <Somnath4x@gmail.com>
2020-01-07 19:21:44 +0530

    Updates

---
+++ precache-manifest.f125481d0cd1f89507c7ed3285be644a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3db43a7c234064a538cc56af5a90bb1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "03d10fa8893860fbda93",
+    "url": "/monster-rolodex/static/css/main.d2bc4d25.chunk.css"
+  },
+  {
+    "revision": "6c241404ab45e1205064",
+    "url": "/monster-rolodex/static/js/2.8061b054.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.8061b054.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03d10fa8893860fbda93",
+    "url": "/monster-rolodex/static/js/main.9bf0cd5f.chunk.js"
+  },
+  {
+    "revision": "32ab0f4b802a835acd9d",
+    "url": "/monster-rolodex/static/js/runtime-main.0d76fd6f.js"
+  }
+]);
